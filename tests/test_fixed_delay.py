e00655beb7e86c5ab8da36d085ef5956d34bbd75
Пищулов Сергей Сергеевич <pishchulov@tochka.com>
2025-03-06 09:08:54 +0500

    fix: add test - only last retry succeed with RetryExecutor

--- tests/test_fixed_delay.py
+++ tests/test_fixed_delay.py
@@ -28,6 +28,22 @@ def test_fixed_delay(respx_mock: respx.MockRouter):
     assert route.call_count == 3
 
 
+@respx.mock()
+def test_fixed_delay_only_last_retry_succeed(respx_mock: respx.MockRouter):
+    route = respx_mock.get("https://example.com")
+    route.side_effect = [
+        httpx.Response(500),
+        httpx.Response(500),
+        httpx.Response(200),
+    ]
+
+    immediate_retry = RetryPolicy().with_max_retries(2).with_delay(0.5)
+    with httpx.Client(transport=RetryTransport(policy=immediate_retry)) as client:
+        res = client.get("https://example.com")
+        assert res.status_code == 200
+    assert route.call_count == 3
+
+
 @pytest.mark.asyncio()
 @respx.mock()
 async def test_async_fixed_delay(respx_mock: respx.MockRouter):
