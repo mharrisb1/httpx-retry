684ab692b912dc8439786c92621ebe496db9c765
Пищулов Сергей Сергеевич <pishchulov@tochka.com>
2025-03-06 09:12:33 +0500

    fix: add test - only last retry succeed with RetryExecutor

--- tests/test_fixed_delay.py
+++ tests/test_fixed_delay.py
@@ -10,8 +10,9 @@ import respx
 from httpx_retry import AsyncRetryTransport, RetryPolicy, RetryTransport
 
 
+@pytest.mark.parametrize("max_retries", [2, 3])
 @respx.mock()
-def test_fixed_delay(respx_mock: respx.MockRouter):
+def test_fixed_delay(respx_mock: respx.MockRouter, max_retries: int):
     route = respx_mock.get("https://example.com")
     route.side_effect = [
         httpx.Response(500),
@@ -19,28 +20,12 @@ def test_fixed_delay(respx_mock: respx.MockRouter):
         httpx.Response(200),
     ]
 
-    immediate_retry = RetryPolicy().with_max_retries(3).with_delay(0.5)
+    immediate_retry = RetryPolicy().with_max_retries(max_retries).with_delay(0.5)
 
     with httpx.Client(transport=RetryTransport(policy=immediate_retry)) as client:
         res = client.get("https://example.com")
         assert res.status_code == 200
 
-    assert route.call_count == 3
-
-
-@respx.mock()
-def test_fixed_delay_only_last_retry_succeed(respx_mock: respx.MockRouter):
-    route = respx_mock.get("https://example.com")
-    route.side_effect = [
-        httpx.Response(500),
-        httpx.Response(500),
-        httpx.Response(200),
-    ]
-
-    immediate_retry = RetryPolicy().with_max_retries(2).with_delay(0.5)
-    with httpx.Client(transport=RetryTransport(policy=immediate_retry)) as client:
-        res = client.get("https://example.com")
-        assert res.status_code == 200
     assert route.call_count == 3
 
 
